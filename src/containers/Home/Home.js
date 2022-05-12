c01aac28c706a4cbef994f28301dc965f989031d
Arya <aryapaikatt000@gmail.com>
2022-05-12 08:23:14 +0530

    user module

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,22 +3,49 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useAppContext } from "../../components/AppContext/AppContext";
 import { getPosts } from "../../services/post.service";
+import { getUsers } from "../../services/users-service";
 
 const Home = () => {
+  const [users, setUsers] = useState([]);
   const [posts, setPosts] = useState([]);
   const { updateAppTitle } = useAppContext();
+ 
   useEffect(() => {
     updateAppTitle("Posts");
   }, [updateAppTitle]);
-
+  
+ 
+  
   useEffect(() => {
     getPosts().then((res) => {
       setPosts(res);
     });
-  }, []);
+  },
+  
+  []);
+  
+  useEffect(() => {
+    getUsers().then((res) => {
+      setUsers(res);
+    });
+  },
+  
+  []);
 
   return (
+    
     <div>
+
+<Typography
+        component="h2"
+        variant="h4"
+        color="green"
+        noWrap
+        sx={{ flexGrow: 1 }}
+        style={{ marginBottom: "20px" }}
+      >
+        Users
+      </Typography>
       <Typography
         component="h2"
         variant="h4"
@@ -43,8 +70,24 @@ const Home = () => {
           </Card>
         </Grid>
       ))}
+      {users.map((val) => (
+        <Grid item style={{ marginBottom: "10px" }} key={val.id} xs={12}>
+          <Card sx={{ height: "100%", display: "flex", flexDirection: "row" }}>
+            <CardContent sx={{ flexGrow: 1 }}>
+              <Link to={`/user/${val.id}`}>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {val.title}
+                </Typography>
+              </Link>
+              <Typography>{val.body}</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      ))}
+      
     </div>
   );
 };
+
 
 export default Home;
