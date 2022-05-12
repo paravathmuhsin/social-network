665e84f6fb5c8d192165586675f8267b86e19451
Vidya Jobin <vidyajobin@gmail.com>
2022-05-12 08:18:31 +0530

    new changes from vidya

--- src/containers/Photos/PhotoDetails.js
+++ src/containers/Photos/PhotoDetails.js
@@ -1,14 +1,13 @@
-
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useAppContext } from "../../components/AppContext/AppContext";
-import { getPhoto } from "../../services/post.service";
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageList from '@mui/material/ImageList';
+import { getPhoto } from "../../services/photos.service";
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
 
 const PhotoDetails = () => {
-
-    const [image, setImage] = useState(null);
+  const [image, setImage] = useState(null);
   const { updateAppTitle } = useAppContext();
   const { id } = useParams();
   useEffect(() => {
@@ -22,20 +21,33 @@ const PhotoDetails = () => {
   }, [id]);
   return (
     <div>
-     <ImageList sx={{ width: 500, height: 450 }} variant="woven" cols={3} gap={8}>
-  {image.map((item) => (
-    <ImageListItem key={item.img}>
-      <img
-        src={item.thumbnailUrl}
-        
-        alt={item.title}
-        loading="lazy"
-      />
-    </ImageListItem>
-  ))}
-</ImageList>
+      {/* <Box sx={{ width: 500, height: 450, overflowY: "scroll" }}>
+        <ImageList variant="masonry" cols={3} gap={8}>
+          {image.map((item) => (
+            <ImageListItem key={item.url}>
+              <img
+                src={`${item.url}?w=248&fit=crop&auto=format`}
+                srcSet={`${item.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+      </Box> */}
+      {image ? (
+        <>
+          <h2>{image.title}</h2>
+          <img
+            src={`${image.url}?w=248&fit=crop&auto=format`}
+            srcSet={`${image.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={image.title}
+            loading="lazy"
+          />
+        </>
+      ) : null}
     </div>
-  )
-}
+  );
+};
 
-export default PhotoDetails
+export default PhotoDetails;
