60df59baf60b667e62706051446e58c1f1c10795
glymalakshmy <glymalakshmi@gmail.com>
2022-06-05 02:05:13 +0530

    Product Listing

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React from "react";
 import { Provider } from "react-redux";
 import AppContext from "./components/AppContext/AppContext";
 import AppRouter from "./components/AppRouter/AppRouter";
-import store from "./store/store";
-
+//import store from "./store/store";
 function App() {
   return (
-    <Provider store={store}>
+    <Provider>
       <AppContext>
         <AppRouter />
       </AppContext>
     </Provider>
   );
 }
-
 export default App;
+
--- src/components/AppContext/AppContext.js
+++ src/components/AppContext/AppContext.js
@@ -4,7 +4,6 @@ const ApplicationContext = createContext(null);
 
 export const useAppContext = ()=> useContext(ApplicationContext)
 
-
 const AppContext = ({ children }) => {
   const [state, setState] = useState({
     appTitle: "",
